a5105b5c43ad5ce90bd3a60bf698366c163cfdf7
Shreya Singh Bhardwaj <168114308+shrybhrdwj@users.noreply.github.com>
2025-02-04 23:37:06 +0530

    Add files via upload

---
+++ unary.java
@@ -0,0 +1,13 @@
+public class unary {
+    public static void main(String[] args) {
+        int x=10;
+        int b=10;
+        System.out.println(x++);
+        System.out.println(++x);
+        System.out.println(x--);
+        System.out.println(--x);
+        System.out.println(x++ + ++x);
+        System.err.println(b++ + b++);
+
+    }
+}
