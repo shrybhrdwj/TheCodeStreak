e80fda1ad6aef7eb54677bf47c239086e7c76b12
Shreya Singh Bhardwaj <168114308+shrybhrdwj@users.noreply.github.com>
2025-02-06 23:30:18 +0530

    Update dictonary.py

---
+++ dictonary.py
@@ -0,0 +1,10 @@
+#Creating a dictonary
+Dict={"1":"Shreya","2":"Singh","3":"Bhardwaj","4":"CSE"}
+print("The Created Dictonary is:")
+print(Dict)
+
+#Accessing using keys
+print("Value of key 2 is:",Dict["2"])
+
+#Accessing using built-in get method
+print("Using the get method:",Dict.get("2"))
